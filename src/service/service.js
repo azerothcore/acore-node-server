d9d1a0a5c0a22fa65ef8dc4f57ce9842f5655180
Yehonal <yehonal.azeroth@gmail.com>
2020-05-13 22:39:27 +0200

    minor fixes

--- src/service/service.js
+++ src/service/service.js
@@ -1,18 +1,10 @@
-import service, {gfShutdownHandler} from '@yield-insight/servicemodule';
-import apolloConfs from '$this/src/graphql';
-import {noderouter, conf} from '$this/src/conf';
-
 /**
  *
  */
 function run() {
-  service.createServer(conf.apiPrefix, apolloConfs, {
-    port: conf.port,
-    routerConf: noderouter,
-    fastifyConf: conf.fastifyConf,
-    gfShutdownHandler,
-    gatewayConf: conf.gatewayConf,
-  });
+  console.log('Hello World');
 }
 
-export {run};
+export {
+  run,
+};
